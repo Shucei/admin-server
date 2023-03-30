60dab8a8dfa02936e375aee0da466a87d649cf1c
Shucei <1003823477@qq.com>
2023-03-30 23:26:02 +0800

    feat:用户登录认证&&接口授权

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,13 @@
 const { User } = require('../model/MongoTable')
 // const Joi = require('joi'); //可以进行验证
 const Bcrypt = require('../utils/md5')
-
+const jwt = require('../utils/jwt')
 class UserInstance {
   static userController = new UserInstance()
 
-  // 注册
+  /**
+ * 注册
+ */
   async getchannel (req, res) {
     let { username, password, email } = req.body
 
@@ -35,26 +37,39 @@ class UserInstance {
     delete user.password
     res.status(201).json({
       data: user,
+      status: 201,
       message: '注册成功'
     })
   }
 
-  // 登录
+  /**
+ * 登录
+ */
   async login (req, res) {
     const { username, password } = req.body;
     try {
-      const user = await User.findOne({ $or: [{ email: username }, { username: username }] });
+      const user = await User.findOne({ $or: [{ email: username }, { username: username }] }); //{ password: 0 } 0 代表不要查询密码
       if (!user) {
-        return res.status(401).json({ message: '无效的用户名或密码' });
+        return res.status(402).json({ status: 402, message: '无效的用户名或密码' });
       }
       const passwordMatches = await Bcrypt.verification(password, user.password);
       if (!passwordMatches) {
-        return res.status(401).json({ message: '无效的用户名或密码' });
+        return res.status(402).json({ status: 402, message: '无效的用户名或密码' });
       }
-      return res.status(200).json({ user, message: '登录成功' });
+
+      user["password"] = undefined
+      const token = jwt.generateToken(user) //存储token
+      return res.status(200).json({ data: user, token, status: 200, message: '登录成功' });
     } catch (error) {
-      return res.status(500).json({ error, message: '出现了一些问题，请稍后再试' });
+      return res.status(500).json({ error, status: 500, message: '出现了一些问题，请稍后再试' });
     }
+  }
+
+  /**
+   * 获取用户列表
+   */
+  getlList (req, res) {
+    res.json('6666')
   }
 }
 module.exports = UserInstance.userController
